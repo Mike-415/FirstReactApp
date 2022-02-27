07d19ddbaf5d4a5cf13c1004fbb12f9be315e474
John Doe <johndoe@Johns-MacBook-Pro.local>
2022-02-27 14:08:06 -0800

    Added hooks to store and delete the task items

--- App.js
+++ App.js
@@ -1,17 +1,47 @@
-import Tasks from './components/Task';
+
+import React, {useState} from 'react';
+import Task from './components/Task';
 import { StatusBar } from 'expo-status-bar';
-import { KeyboardAvoidingView, Platform, StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard} from 'react-native';
 
 export default function App() {
+  /** You use state for variables that change often 
+   *  vs declaring a lot of variables 
+  */
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]); /** gets the previous tasks and appends the new one */
+    setTask(null);
+    console.log(task);
+  }
+
+  const completeTask = (index) => {
+    console.log('completeTask called')
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
   return (
     <View style={styles.container}>
       {/** Today's Tasks */}
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
         <View style={styles.items}>
-          {/**Task items */}
-            <Tasks text={'Another version'}/>
-            <Tasks text={'World!!!'}/>
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity 
+                  key={index}
+                  onPress={ () => completeTask(index)}>
+                  <Task  text={item}/> 
+                </TouchableOpacity>
+              )
+            })
+          }
+
         </View>
       </View> 
       {/** Write a task */}
@@ -22,8 +52,10 @@ export default function App() {
           <TextInput 
             style={styles.input}
             placeholder={'Write a task'}
+            value={task}  //Now you'll see the real-time changes
+            onChangeText={ text => setTask(text)}
           />
-          <TouchableOpacity>
+          <TouchableOpacity onPress={ () => handleAddTask() }>
           <View style={styles.addWrapper}>
             <Text style={styles.addText}>+</Text>
           </View>
